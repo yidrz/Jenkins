cbf9743c760bb17d90411153be34b0697a97cd89
yidrz <root@labvm.vm>
2023-07-09 18:51:51 +0000

    ke 4.

--- sample-app01.sh
+++ sample-app01.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-mkdir tempdir03
-mkdir tempdir03/templates
-mkdir tempdir03/static
+mkdir tempdir04
+mkdir tempdir04/templates
+mkdir tempdir04/static
 
-cp sample_app.py tempdir03/.
-cp -r templates/* tempdir03/templates/.
-cp -r static/* tempdir03/static/.
+cp sample_app.py tempdir04/.
+cp -r templates/* tempdir04/templates/.
+cp -r static/* tempdir04/static/.
 
-echo "FROM python" >> tempdir03/Dockerfile
-echo "RUN pip install flask" >> tempdir03/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir03/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir03/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir03/Dockerfile
-echo "EXPOSE 5050" >> tempdir03/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir03/Dockerfile
+echo "FROM python" >> tempdir04/Dockerfile
+echo "RUN pip install flask" >> tempdir04/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir04/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir04/Dockerfile
+echo "COPY  sample_app.py /home/myapp/" >> tempdir04/Dockerfile
+echo "EXPOSE 5050" >> tempdir04/Dockerfile
+echo "CMD python /home/myapp/sample_app.py" >> tempdir04/Dockerfile
 
-cd tempdir03
-docker build -t sampleapp02 .
-docker run -t -d -p 5050:5050 --name samplerunning02 sampleapp02
+cd tempdir04
+docker run -t -d -p 5050:5050 --name samplerunning02 sampleapp01
 docker ps -a 
