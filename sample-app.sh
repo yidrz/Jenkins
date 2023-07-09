0314539dfa98c3de37b17553eb6838c7a1f9e133
yidrz <root@labvm.vm>
2023-07-09 17:28:22 +0000

    Changed port

--- sample-app.sh
+++ sample-app.sh
@@ -13,10 +13,10 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8090" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp01 .
-docker run -t -d -p 8090:8090 --name samplerunning sampleapp01
+docker run -t -d -p 5050:5050 --name samplerunning01 sampleapp01
 docker ps -a 
